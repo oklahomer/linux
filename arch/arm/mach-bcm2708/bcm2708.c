47243406b81de20390d57e1c430afca7abc769cb
Oklahomer <hagiwara.go@gmail.com>
2014-12-28 20:15:52 +0900

    Add mbed driver

--- arch/arm/mach-bcm2708/bcm2708.c
+++ arch/arm/mach-bcm2708/bcm2708.c
@@ -682,6 +682,34 @@ static struct platform_device bcm2708_i2s_device = {
 };
 #endif
 
+#ifdef CONFIG_SND_BCM2708_SOC_RPI_CODEC_MBED_MODULE
+static struct platform_device snd_rpi_mbed_device = {
+    .name = "snd-rpi-mbed",
+    .id = 0,
+    .num_resources = 0,
+};
+
+static struct i2c_board_info __initdata snd_rpi_mbed_i2c_devices[] = {
+    {
+        I2C_BOARD_INFO("tlv320aic23", 0x1b)
+    },
+};
+#endif
+
+#ifdef CONFIG_SND_BCM2708_SOC_RPI_CODEC_TDA1541A_MODULE
+static struct platform_device snd_rpi_tda1541a_device = {
+    .name = "snd-rpi-tda1541a",
+    .id = 0,
+    .num_resources = 0,
+};
+
+static struct platform_device snd_rpi_tda1541a_codec_device = {
+    .name = "tda1541a-codec",
+    .id = -1,
+    .num_resources = 0,
+};
+#endif
+
 #if defined(CONFIG_SND_BCM2708_SOC_HIFIBERRY_DAC) || defined(CONFIG_SND_BCM2708_SOC_HIFIBERRY_DAC_MODULE)
 static struct platform_device snd_hifiberry_dac_device = {
         .name = "snd-hifiberry-dac",
@@ -912,6 +940,11 @@ void __init bcm2708_init(void)
 
 #ifdef CONFIG_SND_BCM2708_SOC_I2S_MODULE
 	bcm_register_device(&bcm2708_i2s_device);
+#endif
+
+#ifdef CONFIG_SND_BCM2708_SOC_RPI_CODEC_MBED_MODULE
+    bcm_register_device(&snd_rpi_mbed_device);
+    i2c_register_board_info(1, snd_rpi_mbed_i2c_devices, ARRAY_SIZE(snd_rpi_mbed_i2c_devices));
 #endif
 
 #if defined(CONFIG_SND_BCM2708_SOC_HIFIBERRY_DAC) || defined(CONFIG_SND_BCM2708_SOC_HIFIBERRY_DAC_MODULE)
